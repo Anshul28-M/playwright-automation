23bb0a6eca04dfff66e300f9f6892f88f01669dc
Anshul <anshulzerotimesolutions.com>
2026-03-26 12:03:29 +0530

    Initial Playwright project

---
+++ tests/example.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Google search test', async ({ page }) => {
+  await page.goto('https://www.google.com');
+
+  await page.fill('textarea[name="q"]', 'Playwright');
+  await page.press('textarea[name="q"]', 'Enter');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
